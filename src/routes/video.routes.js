20dbff44303d46553022830228d258449e9e5674
Mudit62004 <mudit06verma@gmail.com>
2025-05-06 16:08:16 +0530

    video routes

--- src/routes/video.routes.js
+++ src/routes/video.routes.js
@@ -1,4 +1,25 @@
 import { Router } from "express";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "jsonwebtoken";
-import { deleteVideo, getAllVideos, getVideoById } from "../controllers/video.controller.js";
+import { deleteVideo, getAllVideos, publishVideo, togglePublishStatus, updateVideoDetails } from "../controllers/video.controller.js";
+
+
+const router = Router()
+
+router.route("/publish-video").post(verifyJWT,
+    upload.fields([
+        {
+            name: "videoFile",
+            maxCount:1
+        }
+    ]),
+    publishVideo
+)
+
+router.route("/get-all-videos").get(getAllVideos)
+router.route("/:videoId").get(verifyJWT, getAllVideos)
+router.route("/update-videos/:videoId").post(verifyJWT, upload.single("thumbnail"),updateVideoDetails)
+router.route("/delete-videos/:videoId").delete(verifyJWT, deleteVideo)
+router.route("/toggle-publish-status/:videoId").post(verifyJWT, togglePublishStatus)
+
+export default router
