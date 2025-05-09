a6b4113598c33e5017c6faf57a6df3f56f8dc325
Mudit62004 <mudit06verma@gmail.com>
2025-05-09 23:37:03 +0530

    subscription contoller updation

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -45,5 +45,79 @@ const toggleSubscription = asyncHandler(async(req , res)=> {
     }
 
     //else subscribe the channel
+
+    if (!userSub) {
+        const subscribe = await Subscription.create(
+            {
+                subscriber: user._id,
+                channel: channel._id
+            }
+        )
+        if (!subscribe) {
+            throw new ApiError(500, "Error while subscribing the channel")
+        }
+        return res
+        .status(200)
+        .json(new ApiResponse(200, subscribe, "User subscribed"))
+    }
     
 })
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async(req , res)=> {
+    const channelId = req.params
+
+    if (!getUserChannelSubscribers) {
+        throw new ApiError(400, "Cannot fetch channel id for list")
+    }
+
+    const channel = await User.findById(channelId)
+
+    if (!channel) {
+        throw new ApiError(404, "Channel does not exist")
+    }
+
+    //get the subscribers
+    const subscribers = await Subscription.find(
+        {
+            channel: channel?._id
+        }
+    ).populate('subscriber')
+
+    //get the subscriber count
+    const subscriberCount = await Subscription.countDocunments({
+        channel: channelId
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,{subscriberCount, subscribers}, "Subscribers retrieved successfully"))
+})
+
+//controller to return channel list to which user has subscribed
+
+const getSubscribedChannels = asyncHandler(async(req , res)=> {
+    const subscriberId = req.params
+
+    const subscriptions = await Subscription.find({
+        subscriber: subscriberId
+    })
+    .populate('channel')
+
+    const subscriptionsCount = await Subscription.countDocunments({
+        subscriber: subscriberId
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {subscriptionsCount,subscriptions}, "Subscribed channel fatched successfully"))
+})
+
+export{
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+
+}
+
+
